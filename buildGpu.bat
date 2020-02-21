52cb67eb8209354dd08e9e0e287748c0ffd22a21
Zvyagincev <Zviagintsev@fintech.ru>
2020-02-21 14:06:59 +0300

    Добавил скрипт клонирования репозиториев opencv

--- buildGpu.bat
+++ buildGpu.bat
@@ -11,12 +11,12 @@ cmake -G "Visual Studio 15 2017 Win64" ^
 -DWITH_CUDNN=ON ^
 -DCUDNN_INCLUDE=ON ^
 -DBUILD_SHARED_LIBS=OFF ^
--DOPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" ^
+-DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules" ^
 -DCUDA_ARCH_BIN="6.0 6.1 7.0 7.5" ^
 -DCUDA_FAST_MATH=ON ^
 -DENABLE_FAST_MATH=ON ^
 -DBUILD_WITH_STATIC_CRT=OFF ^
-../../opencv
+../opencv
 
 cmake --build . -j8 --config Release
 
--- buildСpu.bat
+++ buildСpu.bat
@@ -7,10 +7,10 @@ cd buildCpu
 
 cmake -G "Visual Studio 15 2017 Win64" ^
 -DBUILD_SHARED_LIBS=OFF ^
--DOPENCV_EXTRA_MODULES_PATH="../../opencv_contrib/modules" ^
+-DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules" ^
 -DENABLE_FAST_MATH=ON ^
 -DBUILD_WITH_STATIC_CRT=OFF ^
-../../opencv
+../opencv
 
 cmake --build . -j8 --config Release
 
