8be3430307deba75249e5b8c5a4dbd876820ea2e
Alexander <mimus.triurus@yandex.ru>
2020-05-20 14:38:13 +0300

    1. Добавил сборки в конфигурции "VS2015 x64. SDK 8.1. v140. /MT" 2. Добавил список библиотек opencv cpu

--- buildСpuMsvc2015.bat
+++ buildСpuMsvc2015.bat
@@ -6,10 +6,13 @@ md buildCpu
 cd buildCpu
 
 cmake -G "Visual Studio 14 2015 Win64" ^
+-T v140 ^
+-DCMAKE_SYSTEM_VERSION=8 ^
+-DCMAKE_CXX_FLAGS_RELEASE=/MT ^
 -DBUILD_SHARED_LIBS=OFF ^
 -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib/modules" ^
 -DENABLE_FAST_MATH=ON ^
--DBUILD_WITH_STATIC_CRT=OFF ^
+-DBUILD_WITH_STATIC_CRT=ON ^
 ../opencv
 
 cmake --build . --config Release
